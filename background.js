99267a8ace37ee36d5ba58612516be426cd2db9e
Akihiro Matsumura <matsumura.aki@gmail.com>
2011-11-07 16:14:28 +0900

    Play voice

--- background.js
+++ background.js
@@ -1,10 +1,24 @@
 var audio = new Audio();
 audio.src = "audio/happykids.wav";
 audio.load();
+var trans = new Audio();
 
 function playAudio() {
   console.log('------- playAudio()');
   audio.play();
 }
 
-chrome.browserAction.onClicked.addListener(playAudio);
+function playText(text) {
+  var encodeText = encodeURI(text);
+  var url = "http://translate.google.com/translate_tts?ie=UTF-8&q=" + encodeText + "&tl=ja";
+  trans.src = url;
+  trans.load();
+  trans.play();
+}
+
+function clickButton(){
+  playAudio();
+  playText("Hi");
+}
+
+chrome.browserAction.onClicked.addListener(clickButton);
